51c48c654a50d0439421964ba2cfc0a1e586cfcb
Santiago Orozco <santiago.orozco@mvm.com.co>
2023-03-16 18:57:34 -0500

    The model for the gender was created

--- BackEndLS/BackEndLS/Models/Gender.cs
+++ BackEndLS/BackEndLS/Models/Gender.cs
@@ -4,16 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BackEndLS.Models
 {
-    public class Race
+    public class Gender
     {
         [Required]
         [Key]
         public int Id { get; set; }
         [Required]
         [Column(TypeName = "varchar(20)")]
-        public string RaceName { get; set; }
-        [Required]
-        [Column(TypeName = "int")]
-        public int PetTypeId { get; set; }
+        public string GenderName { get; set; }
     }
 }
